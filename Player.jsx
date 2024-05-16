1884540d62d31eedf9c5c7577c76fd5a69bf0966
Garion R L Morgan <77668494+GarionMorgan@users.noreply.github.com>
2024-05-16 12:14:04 -0500

    Updated TicTacaToe game React
    added game board mechanics, switch between player and accept user input for the game board and player names

--- Player.jsx
+++ Player.jsx
@@ -1,11 +1,35 @@
-export default function Player({name, symbol}) {
+import { useState } from 'react';
+
+export default function Player({initialName, symbol, isActive }) {
+	//change the state of the player name
+	const [ playerName, setPlayerName ] = useState(initialName);
+	//change the state of the editing button
+	const [ isEditing, setIsEditing ] = useState(false);
+	//function that is called when user clicks on button
+	function handleEdit() {
+		setIsEditing((editing) => !editing);
+	}
+	//function that is called when user changes player name
+	function handleChange(event) {
+		setPlayerName(event.target.value);
+	}
+	//shows playername 
+	let editablePlayerName = <span className="player-name">{playerName}</span>;
+	//opens up a text box if user clicks on edit button
+	if(isEditing) {
+		editablePlayerName = (
+			<input type="text" required value={playerName} onChange={handleChange} />
+		);
+	}
+
+
 	return(
-		<li>
+		<li className={isActive ? 'active' : undefined }>
 		<span className="player">
-			<span className="player-name">{name}</span>
+			{editablePlayerName}
 			<span className="player-symbol">{symbol}</span>
 		</span>
-		<button>Edit</button>
+		<button onClick={handleEdit}>{isEditing ? 'Save' : 'Edit'}</button>
 		</li>
 	)
 }
